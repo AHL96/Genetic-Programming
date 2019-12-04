50038e4a0468fbf0489b63605f3f5fb0ea9d062c
AHL96 <alexlopez841@gmail.com>
2019-12-03 20:31:33 -0500

    added a physics engine

---
+++ Creature.js
@@ -0,0 +1,37 @@
+class Creature extends Matter.Bodies.rectangle {
+    constructor(world) {
+        super(
+            window.innerWidth * .5, window.innerHeight * .9,
+            10, 10,
+            {
+                isSensor: true,
+                mass: 1
+            }
+        )
+
+        this.genome = new Genome();
+
+        this.geneCounter = 0;
+        this.alive = true;
+        this.fitness = 0;
+
+        if (world) {
+            World.add(world, this);
+        }
+
+    }
+
+    calcFitness = (target) => {
+        let d = Vector.sub(target.position, this.position);
+        d = Vector.magnitude(d);
+        this.fitness = 1 / d;
+        this.fitness = Math.pow(this.fitness, 4);
+    }
+
+    run = () => {
+        var temp = this.genome.sequence[this.geneCounter];
+        Object.assign(this.force, temp);
+        this.geneCounter++;
+    }
+
+}
--- main.js
+++ main.js
@@ -1,19 +1,67 @@
+const Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Vector = Matter.Vector,
+    Events = Matter.Events;
+
+const engine = Engine.create();
+const world = engine.world;
+const target = Bodies.circle(window.innerWidth / 2, window.innerHeight * .1, 10, {
+    isStatic: true
+});
 let population;
-let target;
+
+const obstacle = Bodies.rectangle(
+    window.innerWidth * .5, window.innerHeight * .5,
+    300, 10, {
+    isStatic: true
+});
 
 function setup() {
-  rectMode(CENTER)
-  let w = document.body.offsetWidth;
-  let h = document.body.offsetHeight;
-  createCanvas(w, h);
+    // create an engine
+    noCanvas();
+    engine.world.gravity.y = 0;
 
-  population = new Population();
-  target = new Obstacle(createVector(width - 50, height / 2), 10, 10);
+    // create a renderer
+    const render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: window.innerWidth,
+            height: window.innerHeight,
+            // wireframes: false
+        }
+    });
+
+    population = new Population(engine.world);
+
+    World.add(engine.world, [target, obstacle]);
+
+    Render.run(render);
 }
 
 function draw() {
-  background(200);
-
-  population.run();
-  target.display();
+    population.run();
+    Engine.update(engine);
+    document.getElementById("p1").innerHTML = `cycles left: ${200 - population.cycle}`
+    document.getElementById("p2").innerHTML = `generation: ${population.generation}`
 }
+
+
+Events.on(engine, "collisionStart", function (event) {
+    let pairs = event.pairs
+    for (let i = 0; i < pairs.length; i++) {
+        let bodyA = pairs[i].bodyA
+        let bodyB = pairs[i].bodyB
+        if (bodyA === obstacle || bodyB === obstacle) {
+            if (bodyA != obstacle) {
+                World.remove(world, bodyA);
+            }
+            if (bodyB != obstacle) {
+                World.remove(world, bodyB);
+            }
+        }
+    }
+})
+
